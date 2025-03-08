8a82f1184fc74eebef1cdec8715b082ce9350193
thomfre <10846906+thomfre@users.noreply.github.com>
2025-03-08 15:22:52 +0100

    Persistent field values

--- src/components/checklist/ChecklistField.tsx
+++ src/components/checklist/ChecklistField.tsx
@@ -1,11 +1,30 @@
 import { FormControl, TextField } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ChecklistField = ({ field }: { field: any }): React.ReactElement => {
     const [value, setValue] = useState<any>('');
     const [valueDate, setValueDate] = useState<any>(dayjs());
+
+    useEffect(() => {
+        if (field.type !== 'text') {
+            return;
+        }
+
+        const stored = localStorage.getItem(`checklist-field-${field.title.replaceAll(' ', '-').toLowerCase()}`);
+        if (stored) {
+            setValue(stored);
+        }
+    }, [field]);
+
+    useEffect(() => {
+        if (field.type !== 'text' || value === undefined) {
+            return;
+        }
+
+        localStorage.setItem(`checklist-field-${field.title.replaceAll(' ', '-').toLowerCase()}`, value);
+    }, [field, value]);
 
     if (!field) {
         return <></>;
